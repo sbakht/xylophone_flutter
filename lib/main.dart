48d1c5c4e63f3d9d69a32778c7721df3c6b6a301
Saad Bakht <rotk13@gmail.com>
2020-06-14 13:25:19 -0400

    create sound buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:audioplayers/audio_cache.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
@@ -8,9 +10,43 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Container(
+            child: Column(
+              children: [
+                Note(1, Colors.green),
+                Note(2, Colors.blue),
+                Note(3, Colors.yellow),
+                Note(4, Colors.orange),
+                Note(5, Colors.teal),
+                Note(6, Colors.red),
+                Note(7, Colors.blueGrey),
+              ],
+            ),
+          ),
         ),
       ),
+    );
+  }
+}
+
+class Note extends StatefulWidget {
+  final int note;
+  final Color color;
+  const Note(this.note, this.color);
+
+  @override
+  _NoteState createState() => _NoteState();
+}
+
+class _NoteState extends State<Note> {
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      onPressed: () {
+        final player = AudioCache();
+        player.play('note${this.widget.note}.wav');
+      },
+      color: this.widget.color,
     );
   }
 }
